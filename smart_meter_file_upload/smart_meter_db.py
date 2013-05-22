f6e03cc65204485bfaa0a31e110d2f94a2f6086a
nipunreddevil <nipunreddevil@gmail.com>
2013-05-22 10:38:36 +0530

    Now smart_meter_db runs continuosly rather than from cron.

--- smart_meter_file_upload/smart_meter_db.py
+++ smart_meter_file_upload/smart_meter_db.py
@@ -2,26 +2,26 @@ import web
 import os
 import MySQLdb,sys
 import time,glob
-THRESHOLD_TIME=5
-urls = ('/upload', 'Upload')
+THRESHOLD_TIME=300
 filedir="/home/muc/Desktop/temp/"
 connection = MySQLdb.Connect(host='', user='root', passwd='password', db='smart_meter',local_infile = 1)
 cursor = connection.cursor()
-
-folders=os.listdir(filedir)
-for folder in folders:
-	list_of_files=glob.glob(str(filedir)+str(folder)+str("/*.csv"))
-       	for f in list_of_files: 
-			if int(time.time())-int(os.stat(f).st_mtime)>THRESHOLD_TIME:
-				try:
-					query = "LOAD DATA LOCAL INFILE "+"'"+f+"'"+" INTO TABLE smart_meter_data FIELDS TERMINATED BY ','  IGNORE 1 LINES"			
+while True:
+	time.sleep(THRESHOLD_TIME)
+	folders=os.listdir(filedir)
+	for folder in folders:
+		list_of_files=glob.glob(str(filedir)+str(folder)+str("/*.csv"))
+			for f in list_of_files: 
+				if int(time.time())-int(os.stat(f).st_mtime)>THRESHOLD_TIME:
+					try:
+						query = "LOAD DATA LOCAL INFILE "+"'"+f+"'"+" INTO TABLE smart_meter_data FIELDS TERMINATED BY ','  IGNORE 1 LINES"			
 				
-					cursor.execute(query)
-					connection.commit()
+						cursor.execute(query)
+						connection.commit()
 				
-					os.remove(f)
+						os.remove(f)
 
-				except Exception,e:
-					print e
+					except Exception,e:
+						print e
 			
  
