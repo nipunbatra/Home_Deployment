f96b4107563aa4c6e08f0db01f2e372bf920a24f
nipunreddevil <nipunreddevil@gmail.com>
2013-06-03 13:34:11 +0530

    strng conversion is now stronger

--- raspberry_pi_scripts/sysinfo.py
+++ raspberry_pi_scripts/sysinfo.py
@@ -1,7 +1,7 @@
 import psutil,requests,time,json,subprocess
-SERVER_ADDRESS="http://192.168.16.251:9009/upload"
-TYPE="RASPBERRY_PI"
-ID=1
+SERVER_ADDRESS="http://11.0.0.100:9009/upload"
+TYPE="Server"
+ID=100
 
 while True:
 	cpu_usage=psutil.cpu_percent()
@@ -17,7 +17,7 @@ while True:
 			split_comma=line.split(",")
 		        packet_loss=int(split_comma[2].split("%")[0])
 		if "rtt min/avg/max/mdev" in line:
-			avg_ping=int(line.split("=")[1].split("/")[1])	
+			avg_ping=int(float(line.split("=")[1].split("/")[1]))	
 	client_time=int(time.time())
 	payload={'cpu_usage':cpu_usage,'mem_free':mem_free,'disk_free':disk_free,'client_time':client_time,'type':TYPE,'client_id':ID,'avg_ping':avg_ping,'packet_loss':packet_loss}
 	headers = {'content-type': 'application/json'}
